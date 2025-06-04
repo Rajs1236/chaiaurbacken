fbf7083ac350ce7de298d4e1705c729e91b2e557
Rajs1236 <raj22f069@gmail.com>
2025-06-04 17:59:01 +0000

    vdo user

---
+++ src/models/video.models.js
@@ -0,0 +1,40 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const VideoSchema=new Schema(
+    {
+        videoFile:{
+            type:String, //cloudinary url
+            required:true
+        },
+        thumbnail:{
+            type:String,
+            required:true
+        },
+        description:{
+            type:String,
+            required:true
+        },
+        duration:{
+            type:Number,
+            required:true
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        isPublished:{
+            type:Boolean,
+            default:true
+        },
+        owner:{
+            typr:Schema.type.ObjectId,
+            ref:"USER"
+        }
+
+
+    },{
+        timestamps:true
+    }
+)
+VideoSchema.plugin(mongooseAggregatePaginate)
+export const Video=mongoose.model("Video",VideoSchema)
